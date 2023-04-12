ea6567d66cbaba161f42fdaadc7fe3c451699991
Enrique Martinez <lmartinez128@alumnos.uaq.mx>
2023-04-12 02:17:12 -0600

    Pruebas

---
+++ script.py
@@ -0,0 +1,12 @@
+
+
+'''Hola Diaz, este codigo nada más es para hacer unas cuantas pruebas'''
+
+name = 'KIKS'
+age = '21'
+age = int(age)
+total = age + 10
+
+
+template = f"Hola mi nombre es {name}, tengo {age} años y en 10 años tendre {total} años"
+print(template)
